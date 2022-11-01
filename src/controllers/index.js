03e21adad666d0842d3e539f40182ae1f39a669c
うきこ <87617334+Jeleshkafff@users.noreply.github.com>
2022-11-01 12:13:36 +0300

    the end server

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,3 +2,5 @@
 export * as booksController from "./books";
 export * as authorController from "./authors"
 export * as ganreController from "./ganres"
+export * as readersController from "./readers"
+export * as movingsController from "./movings"
